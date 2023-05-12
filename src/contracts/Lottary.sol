d31cebdc3c0cb1938c38871ce626b804cb537265
George Papadopoulos <papadopoulos.g@gmail.com>
2023-05-12 15:21:08 +0300

    Update Lottary.sol

--- src/contracts/Lottary.sol
+++ src/contracts/Lottary.sol
@@ -19,7 +19,7 @@ contract Lottery {
         return players; // get the array of addresses of the players in the lottery
     }
     function enter() public payable {
-        require(msg.value > fee); // require that the value sent with the transaction is greater than the fee
+        require(msg.value >= fee); // require that the value sent with the transaction is greater than the fee
         // add the address of the player entering the lottery to the array of players
         players.push(payable(msg.sender));
     }

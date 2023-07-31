37e104000976e8cc8e3334a8e68861d5016d8e41
Georgios Papadopoulos <g.papadopoulos@eu.telekom.com>
2023-07-31 15:15:42 +0300

    2nd attempt

--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
-import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+import './App.css';
 
 const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"fee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getUsers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"recipient","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"sendPayment","outputs":[],"stateMutability":"payable","type":"function"}];
-const contractAddress = "0xB5364e95BAC807F262744Dedd87BBF5b70504855"; 
+const contractAddress = "0xB5364e95BAC807F262744Dedd87BBF5b70504855";
 
 function App() {
-  const [provider, setProvider] = useState(null);
-  const [signer, setSigner] = useState(null);
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
   const [contract, setContract] = useState(null);
-  const [users, setUsers] = useState([]);
+  const [recipients, setRecipients] = useState([]);
 
   useEffect(() => {
-    const init = async () => {
-      const provider = await detectEthereumProvider();
-
-      if (provider) {
-        await provider.request({ method: 'eth_requestAccounts' });
-
-        const ethersProvider = new ethers.providers.Web3Provider(provider);
-        const signer = ethersProvider.getSigner();
-
-        const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
-        setProvider(provider);
-        setSigner(signer);
-        setContract(contract);
-      } else {
-        console.log("Please install MetaMask!");
-      }
-    };
-
-    init();
+    initializeWeb3();
   }, []);
 
-  const sendPayment = async () => {
-    const tx = await contract.sendPayment({ value: ethers.utils.parseEther("0.2") });
-    await tx.wait();
-    alert("Payment sent successfully!");
+  const initializeWeb3 = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+        const accounts = await web3Instance.eth.getAccounts();
+        setAccounts(accounts);
+        const contractInstance = new web3Instance.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+        setContract(contractInstance);
+      } catch (error) {
+        console.error('Error connecting wallet:', error);
+      }
+    } else {
+      alert('Please install MetaMask to interact with this dApp.');
+    }
   };
 
-  const getUsers = async () => {
-    const users = await contract.getUsers();
-    setUsers(users);
+  const sendPayment = async () => {
+    if (!web3 || !accounts || accounts.length === 0) {
+      alert('Please connect MetaMask to this dApp.');
+      return;
+    }
+
+    try {
+      await contract.methods.sendPayment().send({
+        from: accounts[0],
+        value: web3.utils.toWei('0.2', 'ether'), // Replace '0.2' with the amount you want to send
+      });
+      const recipients = await contract.methods.getUsers().call();
+      setRecipients(recipients);
+    } catch (error) {
+      console.error('Error sending payment:', error);
+    }
   };
 
   return (
-    <div>
-      <h1>Payment Contract Interface</h1>
-      <button onClick={sendPayment}>Send Payment</button>
-      <button onClick={getUsers}>Get Users</button>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>{user}</li>
-        ))}
-      </ul>
+    <div className="App">
+      <h1>Payment dApp</h1>
+      {accounts.length === 0 ? (
+        <button onClick={initializeWeb3}>Connect Wallet</button>
+      ) : (
+        <>
+          <button onClick={sendPayment}>Send Payment</button>
+          <h2>Recipients:</h2>
+          <ul>
+            {recipients.map((recipient, index) => (
+              <li key={index}>{recipient}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }

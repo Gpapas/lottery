2d93cf9271b2086729483a17bd2663042741f67a
George Papadopoulos <papadopoulos.g@gmail.com>
2023-07-31 14:50:20 +0300

    sms

--- src/App.js
+++ src/App.js
@@ -1,66 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import './App.css';
+import { ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
 
-const CONTRACT_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"enter","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"fee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRandomNumber","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"lottery","type":"uint256"}],"name":"getWinnerByLottery","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"lotteryHistory","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lotteryId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pickWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"players","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"}] // Replace with the ABI of your Lottery contract
-const CONTRACT_ADDRESS = '0xCD531A31C9cD4D11f4c3afA7C7FDaE64150c29C1';
+const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"fee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getUsers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"recipient","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"sendPayment","outputs":[],"stateMutability":"payable","type":"function"}];
+const contractAddress = "0xB5364e95BAC807F262744Dedd87BBF5b70504855"; 
 
 function App() {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState([]);
-  const [lotteryContract, setLotteryContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    if (web3 && accounts.length > 0) {
-      const lotteryInstance = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-      setLotteryContract(lotteryInstance);
-    }
-  }, [web3, accounts]);
+    const init = async () => {
+      const provider = await detectEthereumProvider();
 
-  const connectWallet = async () => {
-    if (window.ethereum) {
-      try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const web3Instance = new Web3(window.ethereum);
-        setWeb3(web3Instance);
-        const accounts = await web3Instance.eth.getAccounts();
-        setAccounts(accounts);
-      } catch (error) {
-        console.error('Error connecting wallet:', error);
+      if (provider) {
+        await provider.request({ method: 'eth_requestAccounts' });
+
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
+        const signer = ethersProvider.getSigner();
+
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        setProvider(provider);
+        setSigner(signer);
+        setContract(contract);
+      } else {
+        console.log("Please install MetaMask!");
       }
-    } else {
-      alert('Please install MetaMask to interact with this dApp.');
-    }
+    };
+
+    init();
+  }, []);
+
+  const sendPayment = async () => {
+    const tx = await contract.sendPayment({ value: ethers.utils.parseEther("0.2") });
+    await tx.wait();
+    alert("Payment sent successfully!");
   };
 
-  const enterLottery = async () => {
-    if (!web3 || !accounts || accounts.length === 0) {
-      alert('Please connect MetaMask to this dApp.');
-      return;
-    }
-
-    const fee = await lotteryContract.methods.fee().call();
-    await lotteryContract.methods.enter().send({
-      from: accounts[0],
-      value: fee,
-    });
-  };
-
-  const pickWinner = async () => {
-    await lotteryContract.methods.pickWinner().send({ from: accounts[0] });
+  const getUsers = async () => {
+    const users = await contract.getUsers();
+    setUsers(users);
   };
 
   return (
-    <div className="App">
-      <h1>Lottery dApp</h1>
-      {accounts.length === 0 ? (
-        <button onClick={connectWallet}>Connect Wallet</button>
-      ) : (
-        <>
-          <button onClick={enterLottery}>Enter Lottery</button>
-          <button onClick={pickWinner}>Pick Winner</button>
-        </>
-      )}
+    <div>
+      <h1>Payment Contract Interface</h1>
+      <button onClick={sendPayment}>Send Payment</button>
+      <button onClick={getUsers}>Get Users</button>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
     </div>
   );
 }
